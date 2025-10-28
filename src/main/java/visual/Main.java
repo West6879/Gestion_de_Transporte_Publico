a71dbed15c6b506daa7e9ba70ddc69c566f14a5d
West6879 <west13731@gmail.com>
2025-10-28 14:43:43 -0400

    proyecto seteado con maven para javaFX

--- src/main/java/visual/Main.java
+++ src/main/java/visual/Main.java
@@ -1,14 +1,23 @@
 package visual;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/IngresoEstaciones.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         Scene scene = new Scene(root, 400, 300);
         primaryStage.setScene(scene);
         primaryStage.setTitle("My JavaFX App");
